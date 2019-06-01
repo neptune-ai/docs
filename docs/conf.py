28f18de9f3201d56190d5a0e2085ccbfbd29654f
kamil-kaczmarek <kamil.kaczmarek@neptune.ml>
2019-06-01 17:36:35 +0200

    conf correction

--- docs/conf.py
+++ docs/conf.py
@@ -47,6 +47,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'navigation_depth': 1,
+    'prev_next_buttons_location': 'bottom',
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
