295cbd1061cf9bb1d9e0b805557ad1d8ea80019b
Hubert Jaworski <hubert.jaworski@neptune.ml>
2019-06-05 11:07:52 +0200

    Adjustments

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,4 @@ FROM nginx:stable
 
 COPY docs/_build/html/ /usr/share/nginx/html
 
-RUN mkdir /app
-
 ADD docker/nginx.conf /etc/nginx/conf.d/default.conf
-ADD docker/run_app.sh /app
-
-CMD /app/run_app.sh
--- cicd/get_config.py
+++ cicd/get_config.py
@@ -8,20 +8,17 @@ from git import Repo
 from jinja2 import Environment, FileSystemLoader
 
 
-def create_values(env, registry, image):
-    with open("dependencies.yaml", 'r') as stream:
-        data = yaml.safe_load(stream)
-        template_env = Environment(loader=FileSystemLoader(data["environment"][env]["templates"]))
-        template = template_env.get_template("neptune-docs.yaml.j2")
-        return template.render(image=image, registry=registry)
-
-
-def main():
+if __name__ == "__main__":
     env = sys.argv[1]
     registry = sys.argv[2]
     image = sys.argv[3]
 
-    print(create_values(env, registry, image))
+    template_directory = "cicd/templates/stage"
 
-if __name__ == "__main__":
-    main()
+    if env == "prod":
+        template_directory = "cicd/templates/prod"
+
+    template_env = Environment(loader=FileSystemLoader(template_directory))
+    template = template_env.get_template("neptune-docs.yaml.j2")
+
+    print(template.render(image=image, registry=registry))
