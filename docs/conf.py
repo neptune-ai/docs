95c413f7af28a6b3ea13a2fa5f9de41736decccb
Hubert Jaworski <hubert.jaworski@neptune.ml>
2019-06-25 18:38:53 +0200

    Add favicon (#8)
    * Add favicon
    
    * moved icon to _static
    
    * formatting

--- docs/conf.py
+++ docs/conf.py
@@ -58,3 +58,5 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_favicon = '_static/images/others/favicon.ico'
