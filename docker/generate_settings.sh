624808dfd699d380fd50130dca7e4525bb7debe6
lukasz.walkiewicz <lukasz.walkiewicz@neptune.ml>
2019-09-11 12:27:29 +0200

    Add hotjar & google analytics (#19)

---
+++ docker/generate_settings.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+# Copyright (c) 2019, Neptune Labs
+#
+# Sets various options from environemnt variables to docker-config.js
+#
+
+set -e
+
+# Settings
+WWWROOT="/usr/share/nginx/html"
+CONFIG_FILE="$WWWROOT/docker-config.js"
+
+echo "generating settings for docs from environment variables"
+
+# Use default values if variables are not set
+: ${IS_PRODUCTION:=false}
+
+echo "
+  window.dockerConfig = {
+    isProduction: $IS_PRODUCTION
+  };
+" > $CONFIG_FILE
+
+echo "See generated configuration:"
+cat $CONFIG_FILE
+
