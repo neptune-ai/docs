0d8d86eb81790304dd6c0373b3bce8eb2a85360b
jakubczakon <czakon.jakub@gmail.com>
2020-09-07 12:22:51 +0200

    update

--- create_docs.sh
+++
@@ -1,5 +0,0 @@
-rm -rf docs/_build/html
-cd docs
-make html
-cd _build/html
-python3 -m http.server
--- get_urls.py
+++
@@ -1,6 +0,0 @@
-from usp.tree import sitemap_tree_for_homepage
-
-tree = sitemap_tree_for_homepage('https://docs.neptune.ai/')
-
-for page in tree.all_pages():
-    print(page)
